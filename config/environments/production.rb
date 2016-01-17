c33c774dff89344124daaa92b53bfcb0a604c00d
Dev-And <raptor131296@gmail.com>
2016-01-17 22:51:25 +0200

    host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address: 'smtp.gmail.com',
       port: 587,
-      domain: 'https://salty-wildwood-5561.herokuapp.com/',
+      domain: 'heroku.com',
       authentication: 'plain',
       enable_starttls_auto: true,
       user_name: ENV["GMAIL_USERNAME"],
